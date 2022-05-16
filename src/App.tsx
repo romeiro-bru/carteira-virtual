c5f8d8353dd644ab4e07488d0b9baceea4ec58e2
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-05-16 19:17:10 -0300

    Import components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import './App.css';
+import { Form } from './Components/Form';
+import { Nav } from './Components/Nav';
+import { Header } from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Carteira Virtual</h1>
+      <Header />
+      <div className="flex">
+        <section className="menu">
+          <aside>
+            <h2>Saldo</h2>
+            <ul>
+              <li>BRL: R$ 100.000</li>
+              <li>BTC: ฿ 0.00</li>
+              <li>BUSD: 0.00</li>
+              <li>Preço BTC: R$ 152,298.67</li>
+            </ul>
+          </aside>
+          <Nav />
+        </section>
+        <main>
+          <h1>Compra de Criptomoedas</h1>
+          <Form />
+        </main>
+      </div>
     </div>
   );
 }

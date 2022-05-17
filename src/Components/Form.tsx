6c122c9c069e3d411656dfa9da7ba475437a35ee
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-05-17 16:32:51 -0300

    Add more fields, Receive props, Type props

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,14 +1,39 @@
-export function Form() {
+type AccountInitVal = {
+  accountInitVal: number;
+}
+
+export function Form({ accountInitVal }: AccountInitVal) {
+
   return (
     <form className="relative">
-      <label className="block" htmlFor="bitcoin-venda">Você comprará:</label>
-      <input className="border border-solid border-slate-200 py-3 px-6 my-2 mx-0.5 text-lg" type="number" />
-      <span className="py-4 px-5 border">BTC</span>
 
-      <label className="block" htmlFor="busd-venda">Você comprará:</label>
-      <input className="border border-solid border-slate-200 py-3 px-6 my-2 mx-0.5 text-lg" type="number" />
-      <span className="p-4 border">BUSD</span>
+      <div className="flex">
+        <div>
+          <label className="block" htmlFor="bitcoin-compra">Você comprará:</label>
+          <input className="w-2/4 border border-solid border-slate-200 p-2 my-2 mx-0.5 text-lg" type="number" />
+          <span className="p-3 border">BRL</span>
+        </div>
+        <div>
+          <label className="block" htmlFor="bitcoin-recebe">Você receberá:</label>
+          <input className="w-2/4 border border-solid border-slate-200 p-2 my-2 mx-0.5 text-lg" type="number" />
+          <span className="p-3 border">BTC</span>
+        </div>
+      </div>
 
+
+      <div className="flex">
+        <div>
+          <label className="block" htmlFor="busd-compra">Você comprará:</label>
+          <input className="w-2/5 border border-solid border-slate-200 p-2 my-2 mx-0.5 text-lg" type="number" />
+          <span className="p-3 border">BRL</span>
+        </div>
+
+        <div>
+          <label className="block" htmlFor="bitcoin-recebe">Você receberá:</label>
+          <input className="w-2/5 border border-solid border-slate-200 p-2 my-2 mx-0.5 text-lg" type="number" />
+          <span className="p-3 border">BUSD</span>
+        </div>
+      </div>
       <button className="absolute top-64 left-0 py-4 px-8 rounded-full bg-[#68ed9b] hover:bg-[#5ace7e] duration-100 text-slate-800 font-bold">Comprar</button>
     </form>
   );

a52da65671633db45052fdaac79f3716ed2721e6
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-05-17 16:42:48 -0300

    Change background color

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <header className="flex justify-around items-center py-5 mb-8 bg-slate-900">
+    <header className="flex justify-around items-center py-5 mb-8 bg-[#5ace7e]">
       <span className="text-white">Logo</span>
       <button className="logout bg-transparent text-slate-50 font-thin ">logout</button>
     </header>

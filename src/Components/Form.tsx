84d1573d225038c78c0321437cb7617a4741cfb7
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-05-17 11:37:32 -0300

    Add tailwind classes

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,15 +1,15 @@
 export function Form() {
   return (
-    <form>
-      <label htmlFor="bitcoin-venda">Você comprará:</label>
-      <input type="text" />
-      <span>BTC</span>
+    <form className="relative">
+      <label className="block" htmlFor="bitcoin-venda">Você comprará:</label>
+      <input className="border border-solid border-slate-200 py-3 px-6 my-2 mx-0.5 text-lg" type="number" />
+      <span className="py-4 px-5 border">BTC</span>
 
-      <label htmlFor="busd-venda">Você comprará:</label>
-      <input type="text" />
-      <span>BUSD</span>
+      <label className="block" htmlFor="busd-venda">Você comprará:</label>
+      <input className="border border-solid border-slate-200 py-3 px-6 my-2 mx-0.5 text-lg" type="number" />
+      <span className="p-4 border">BUSD</span>
 
-      <button>Comprar</button>
+      <button className="absolute top-64 left-0 py-4 px-8 rounded-full bg-[#68ed9b] hover:bg-[#5ace7e] duration-100 text-slate-800 font-bold">Comprar</button>
     </form>
   );
 }
--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,10 +1,10 @@
 export function Nav() {
   return (
-    <nav>
-      <ul>
-        <li>Compra</li>
-        <li>Venda</li>
-        <li>Transações</li>
+    <nav className="mt-3 bg-white rounded-md shadow-md text-slate-700 text-left">
+      <ul className="list-none p-0">
+        <li className="py-3 px-5 border-b-2">Compra</li>
+        <li className="py-3 px-5 border-b-2">Venda</li>
+        <li className="py-3 px-5">Transações</li>
       </ul>
     </nav>
   );

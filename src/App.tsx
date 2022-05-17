b71b298c74fc08c204bdde508d6293ac1ea8e4b8
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-05-17 11:37:10 -0300

    Remove App css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { Form } from './Components/Form';
 import { Nav } from './Components/Nav';
 import { Header } from './Components/Header';
@@ -8,20 +7,20 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <div className="flex">
+      <div className="flex flex-wrap justify-evenly">
         <section className="menu">
-          <aside>
-            <h2>Saldo</h2>
-            <ul>
-              <li>BRL: R$ 100.000</li>
-              <li>BTC: ฿ 0.00</li>
-              <li>BUSD: 0.00</li>
-              <li>Preço BTC: R$ 152,298.67</li>
+          <aside className="bg-white rounded-md shadow-md text-left text-slate-700">
+            <h2 className="m-0 pt-4 pl-4">Saldo</h2>
+            <ul className="list-none p-0">
+              <li className="py-3 px-5">BRL: R$ 100.000</li>
+              <li className="py-3 px-5">BTC: ฿ 0.00</li>
+              <li className="py-3 px-5">BUSD: 0.00</li>
+              <li className="py-3 px-5">Preço BTC: R$ 152,298.67</li>
             </ul>
           </aside>
           <Nav />
         </section>
-        <main>
+        <main className="w-2/4 py-4 px-8 bg-white rounded-md shadow-md text-slate-700 text-left">
           <h1>Compra de Criptomoedas</h1>
           <Form />
         </main>

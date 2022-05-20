6bb6c160fbda78fe2e623ac2985b211dfa935324
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-05-20 12:44:14 -0300

    Convert api response to number

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ type ApiBtc = {
 };
 
 function App() {
-  const [accBalance, setAccBalance] = useState<number>(100000)
-  const [btcPrice, setBtcPrice] = useState<string>("0")
+  const [accBalance, setAccBalance] = useState(100000)
+  const [btcPrice, setBtcPrice] = useState(0)
   const [btcAmount, setBtcAmount] = useState(0)
   const [busdAmount, setBusdAmount] = useState(0)
 
@@ -23,7 +23,7 @@ function App() {
     const fetchBtcPrice = async () => {
       try {
         const response = await axios.get<ApiBtc>(apiBtc)
-        setBtcPrice(response.data.ticker.last)
+        setBtcPrice(parseFloat(response.data.ticker.last))
       } catch (error) {
         console.log(error)
       }
@@ -31,7 +31,7 @@ function App() {
     fetchBtcPrice()
   }, [])
 
-  const transformCurrency = parseInt(btcPrice).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+  const transformCurrency = btcPrice.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
 
   return (
     <div className="App">
@@ -42,17 +42,18 @@ function App() {
             <h2 className="font-medium text-lg pt-4 pl-4">Saldo</h2>
             <ul className="list-none p-0">
               <li className="flex justify-between py-3 px-5">BRL: <div>{accBalance.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</div></li>
-              <li className="flex justify-between py-3 px-5">BTC:  <div>฿ {parseFloat(btcAmount.toFixed(6))}</div></li>
+              <li className="flex justify-between py-3 px-5">BTC:  <div>฿ {btcAmount.toFixed(6)}</div></li>
 
-              <li className="flex justify-between pt-3 pb-8 px-5">BUSD: <div>$ {parseFloat(busdAmount.toFixed(3))}</div></li>
+              <li className="flex justify-between pt-3 pb-8 px-5">BUSD: <div>$ {busdAmount.toFixed(3)}</div></li>
               <li className="flex justify-between text-xs text-slate-500 py-5 px-2 border-t-2">Preço BTC: <div>{transformCurrency}</div></li>
             </ul>
           </aside>
           <Nav />
         </section>
         <main className="relative w-2/4 py-4 px-8 bg-white rounded-md shadow-md text-slate-700 text-left">
-          <h1 className="mb-9 text-xl font-medium">Compra de Criptomoedas</h1>
-          <Form setBusdAmount={setBusdAmount} setBtcAmount={setBtcAmount} btcPrice={btcPrice} setAccBalance={setAccBalance} accBalance={accBalance} />
+          <h1 className="mb-9 text-xl font-medium">Compra e Venda de Criptomoedas</h1>
+          <Form busdAmount={busdAmount} btcAmount={btcAmount} setBusdAmount={setBusdAmount} setBtcAmount={setBtcAmount} btcPrice={btcPrice} setAccBalance={setAccBalance} accBalance={accBalance} />
+
         </main>
       </div>
     </div>

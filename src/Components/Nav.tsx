2d42c8ecb14fe9dec6ce3166f74d0c2f4c6c198b
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-05-20 14:27:11 -0300

    Change list item

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -2,8 +2,7 @@ export function Nav() {
   return (
     <nav className="mt-5 bg-white rounded-md shadow-md text-slate-700 text-left">
       <ul className="list-none p-0">
-        <li className="py-3 px-5 border-b-2">Compra</li>
-        <li className="py-3 px-5 border-b-2">Venda</li>
+        <li className="py-3 px-5 border-b-2">Compra e Venda</li>
         <li className="py-3 px-5">Transações</li>
       </ul>
     </nav>

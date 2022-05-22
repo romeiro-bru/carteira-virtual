10b4e8aed14fb54c69603734d441e61113436477
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-05-22 16:29:08 -0300

    Add link to login page

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export function Header() {
   return (
     <header className="flex justify-around items-center py-5 bg-secondary-color">
       <span className="font-bold text-white">Carteira Virtual</span>
-      <button className="logout bg-transparent text-slate-50">logout</button>
+      <button className="logout bg-transparent text-slate-50">
+        <Link to="/login">logout</Link>
+      </button>
     </header>
   );
 }

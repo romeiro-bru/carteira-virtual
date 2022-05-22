e2788019b612c6dac858627a4c553b2cb7d44a0b
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-05-22 16:34:03 -0300

    Move button wrap with Link

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,9 +4,11 @@ export function Header() {
   return (
     <header className="flex justify-around items-center py-5 bg-secondary-color">
       <span className="font-bold text-white">Carteira Virtual</span>
-      <button className="logout bg-transparent text-slate-50">
-        <Link to="/login">logout</Link>
-      </button>
+      <Link to="/login">
+        <button className="logout bg-transparent text-slate-50">
+          logout
+        </button>
+      </Link>
     </header>
   );
 }

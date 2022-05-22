f65d40ca25f9b515d0cde5483edc1505e408d0b4
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-05-22 15:47:07 -0300

    Remove margin bottom

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <header className="flex justify-around items-center py-5 mb-8 bg-secondary-color">
+    <header className="flex justify-around items-center py-5 bg-secondary-color">
       <span className="font-bold text-white">Carteira Virtual</span>
       <button className="logout bg-transparent text-slate-50">logout</button>
     </header>

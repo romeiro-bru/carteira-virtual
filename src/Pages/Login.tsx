60d61c22930cf36e879a62439b7354c5f868d18c
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-05-23 13:07:40 -0300

    Add password to input field

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -7,7 +7,7 @@ export function Login() {
           <input placeholder="e-mail" type="text" id="e-mail" className="w-10/12 rounded border border-solid border-slate-200 p-2 my-2 text-lg" />
 
           <label htmlFor="password" className="block">Senha</label>
-          <input placeholder="senha" type="text" id="password" className="w-10/12 rounded border border-solid border-slate-200 p-2 my-2 text-lg" />
+          <input placeholder="senha" type="password" id="password" className="w-10/12 rounded border border-solid border-slate-200 p-2 my-2 text-lg" />
         </div>
         <button className="disabled:opacity-60 absolute top-2/3 left-3 w-11/12 py-4 px-8 rounded-full bg-primary-color hover:bg-secondary-color duration-100 text-slate-800 font-bold">
           Login

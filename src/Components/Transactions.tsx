780ba60d9ac2447e5d7ae337c82935258d6c5ab5
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-05-23 13:11:01 -0300

    Create transactions component

---
+++ src/Components/Transactions.tsx
@@ -0,0 +1,7 @@
+export function Transactions() {
+  return (
+    <main className="relative w-2/4 py-4 px-8 bg-white rounded-md shadow-md text-slate-700 text-left">
+      <h1 className="mb-9 text-xl font-medium">Transferências</h1>
+    </main>
+  );
+}

fd7026ff728a4eedb8cc48136a6696060424b459
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-05-25 21:54:19 -0300

    Add link

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 export function Header() {
   return (
     <header className="flex justify-around items-center py-5 bg-secondary-color">
-      <span className="font-bold text-white">Carteira Virtual</span>
+      <span className="font-bold text-white">
+        <Link to="/">
+          Carteira Virtual
+        </Link>
+      </span>
       <Link to="/login">
         <button className="logout bg-transparent text-slate-50">
           logout

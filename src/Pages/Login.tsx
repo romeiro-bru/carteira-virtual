85ff82461e73cd776233cd1513ed4305aadd7714
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-05-26 18:46:09 -0300

    Add link to login page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export function Login() {
   return (
     <div className="flex justify-center h-screen">
@@ -9,9 +11,11 @@ export function Login() {
           <label htmlFor="password" className="block">Senha</label>
           <input placeholder="senha" type="password" id="password" className="w-10/12 rounded border border-solid border-slate-200 p-2 my-2 text-lg" />
         </div>
-        <button className="disabled:opacity-60 absolute top-2/3 left-3 w-11/12 py-4 px-8 rounded-full bg-primary-color hover:bg-secondary-color duration-100 text-slate-800 font-bold">
-          Login
-        </button>
+        <Link to="/">
+          <button className="disabled:opacity-60 absolute top-2/3 left-3 w-11/12 py-4 px-8 rounded-full bg-primary-color hover:bg-secondary-color duration-100 text-slate-800 font-bold">
+            Login
+          </button>
+        </Link>
       </form>
     </div>
   );

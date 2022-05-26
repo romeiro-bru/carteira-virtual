89011b0ed31c4cdea91a535c26d930ab9010f301
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-05-26 18:07:37 -0300

    Change style

--- src/Components/Transactions.tsx
+++ src/Components/Transactions.tsx
@@ -1,16 +1,16 @@
-type Data = {
-  data: Array<{ id: number, btc: number, busd: number, balance: number }>
+type AccBalance = {
+  wallet: Array<{ id: number, btc: number, busd: number, balance: number }>;
 }
 
-export function Transactions({ data }: Data) {
+export function Transactions({ wallet }: AccBalance) {
   return (
     <main className="relative w-2/4 py-4 px-8 bg-white rounded-md shadow-md text-slate-700 text-left">
       <h1 className="mb-9 text-xl font-medium">Transações</h1>
       <ul>
-        {data !== undefined ? data.map((item, i) => (
-          <li key={i} className="flex justify-between flex-wrap p-2 border-t-2 border-slate-300 even:bg-slate-200" >
-            <span>฿ {item.btc}</span>
-            <span>$ {item.busd}</span>
+        {wallet !== undefined ? wallet.map(item => (
+          <li key={item.id}>
+            <div className="flex justify-between flex-wrap p-2 border-t-2 border-slate-300 even:bg-slate-200" >$ {item.busd}</div>
+            <div className="flex justify-between flex-wrap p-2 border-t-2 border-slate-300 even:bg-slate-200" >฿ {item.btc}</div>
           </li>
         )) : ""}
       </ul>
